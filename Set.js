3afe10915119c103c68b8189c8654cfb2dcfa09f
David Nolen <david.nolen@gmail.com>
2009-08-11 16:38:32 -0400

    Initial commit.

---
+++ Set.js
@@ -0,0 +1,106 @@
+(function() {
+
+function $normalize(v) {
+  if($type(v) == "array") return v.normalize();
+  if($type(v) == "hash") v = v.getClean();
+  if($type(v) == "object") {
+    var result = [];
+    var sortArray = [];
+    for(var k in v) sortArray.push(k);
+    sortArray.sort();
+    for(var i = 0, l = sortArray.length; i < l; i++) {
+      var k = sortArray[i];
+      result.push([k, $normalize(v[k])]);
+    }
+    return result;
+  }
+  return v;
+}
+
+window.$hash = function(v) {
+  return JSON.encode($normalize(v));
+};
+
+Array.implement({
+  normalize: function() {
+    var result = [];
+    for(var i = 0, l = this.length; i < l; i++) result.push($normalize(this[i]));
+    return result;
+  }
+});
+
+window.Set = new Class({
+  initialize: function(ary) {
+    this.isset = true;
+    this.rep = {};
+    if(ary && !ary.isset) {
+      for(var i = 0, len = ary.length; i < len; i++) this.rep[$hash(ary[i])] = ary[i];
+    } else if(ary) {
+      this.rep = ary.rep;
+    }
+  },
+  
+  put: function(v) {
+    this.rep[$hash(v)] = v;
+  },
+  
+  get: function(v) {
+    return this.rep[$hash(v)];
+  },
+  
+  remove: function(v) {
+    delete this.rep[$hash(v)];
+  },
+  
+  intersection: function(set) {
+    var result = new Set();
+    set = new Set(set);
+    for(var k in this.rep) {
+      var v = this.rep[k], hashed = $hash(v);
+      if(set.rep[hashed]) result.rep[hashed] = v;
+    }
+    return result;
+  },
+  
+  aintersection: function(set){ return this.intersection(set).toArray(); },
+
+  difference: function(set) {
+    var result = new Set();
+    set = new Set(set);
+    for(var k in this.rep) {
+      var v = this.rep[k], hashed = $hash(v);
+      if(!set.rep[hashed]) result.rep[hashed] = v;
+    }
+    for(var k in set.rep) {
+      var v = set.rep[k], hashed = $hash(v);
+      if(!this.rep[hashed]) result.rep[hashed] = v;
+    }
+    return result;
+  },
+  
+  adifference: function(set){ return this.difference(set).toArray(); },
+  
+  union: function(set) {
+    var result = new Set();
+    set = new Set(set);
+    for(var k in this.rep) result.rep[k] = this.rep[k];
+    for(var k in set.rep) result.rep[k] = set.rep[k];
+    return result;
+  },
+  
+  aunion: function(set) { return this.union(set).toArray(); },
+  
+  toArray: function() {
+    var result = [];
+    for(var k in this.rep) result.push(this.rep[k]);
+    return result;
+  },
+  
+  isEqual: function(set) {
+    set = new Set(set);
+    for(var k in this.rep) if(!set.rep[k]) return false;
+    return true;
+  }
+});
+
+})()
---
+++ test.js
@@ -0,0 +1,78 @@
+window.addEvent('domready', init);
+
+function createArray(n)
+{
+  var result = []
+  for(var i = 0; i < n; i++)
+  {
+    result.push("bar");
+  }
+  result[n/2] = "foo";
+  return result;
+}
+
+function createSet(n)
+{
+  var result = []
+  for(var i = 0; i < n; i++)
+  {
+    result.push("bar");
+  }
+  return new Set(result);
+}
+
+function testArrayContains()
+{
+  console.log('testArrayContains setup:');
+  console.time('setup');
+  var ary = createArray(10000);
+  console.timeEnd('setup');
+  console.log('contains:');
+  console.time("contains");
+  for(var i = 0; i < 5000; i++)
+  {
+    ary.contains("foo");
+  }
+  console.timeEnd("contains");
+}
+
+function testSetGet()
+{
+  console.log('testSetGet setup:');
+  console.time("setup");
+  var set = createSet(10000);
+  console.timeEnd("setup");
+  console.log('get:');
+  console.time("get");
+  for(var i = 0; i < 5000; i++)
+  {
+    set.get("foo");
+  }
+  console.timeEnd('get');
+}
+
+function init()
+{
+  console.log('ready');
+  testArrayContains();
+  testSetGet();
+  
+  var set0 = new Set([1, 2, 1, "dog", "bird", "dog", "dog", "cat"]);
+  console.log("set0:");
+  console.log(set0.toArray());
+  
+  var set1 = new Set([1, 2, [3, 4]]);
+  var set2 = new Set([1, [3, 4], 5]);
+  console.log("set1 set2 intersection:");
+  console.log(set1.aintersection(set2));
+  
+  var set3 = new Set([{first:"Bob", last:"Smith"}, {first:"Mary", last:"Smith"}]);
+  var set4 = new Set([{first:"Tim", last:"Smith"}, {first:"Mary", last:"Smith"}]);
+  console.log("set3 set4 difference:");
+  console.log(set3.adifference(set4));
+  
+  var set5 = new Set(["dog", "cat", "bird"]);
+  var set6 = new Set(["zebra", "lion", "gazelle"]);
+  console.log('set5 set6 union:');
+  console.log(set5.aunion(set6));
+}
